6959de6a0aeeb22b759c8bcda0225932373b18e8
parice esperou <pesperou@gmail.com>
2021-10-01 10:33:19 +0200

    refactoring classe in Article.php

--- Article.php
+++ Article.php
@@ -1,24 +1,21 @@
 <?php
 
-class Article
+class Article extends Database
 {     //Création de la methode getarticles (liste d'articles)
     public function getArticles()
     {
-        $db = new Database();
-        $connection = $db->getConnection();
-        $result = $connection->query('SELECT id, title, content, author, createdAt FROM article ORDER BY id DESC');
-        return $result;
+        
+        
+        $sql ='SELECT id, title, content, author, createdAt FROM article ORDER BY id DESC';
+        return $this->createQuery($sql);
     }
 
 
     public function getArticle($articleId)//idem methode pour 1
     {
-        $db = new Database();
-        $connection = $db->getConnection();
-        $result = $connection->prepare('SELECT id, title, content, author, createdAt FROM article WHERE id = ?');
-        $result->execute([
-            $articleId //on passe ce parametre $articleId  pour appeller 1 seul article
-        ]);
-        return $result;
+        
+        $sql ='SELECT id, title, content, author, createdAt FROM article WHERE id = ?';
+          
+        return $this->createQuery($sql,[$articleId]);
     }
 }
