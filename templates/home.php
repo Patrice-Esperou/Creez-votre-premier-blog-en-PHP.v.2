940c467ac2cd523c8bf93025d2ea536fb51501f9
parice esperou <pesperou@gmail.com>
2021-10-02 10:20:38 +0200

    delete call autoload and change way in link

--- templates/home.php
+++ templates/home.php
@@ -1,9 +1,7 @@
 <?php
-require '../vendor/Autoload.php';
-
-
 use App\src\DAO\ArticleDAO
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -22,7 +20,7 @@ use App\src\DAO\ArticleDAO
     {
         ?>
         <div>
-            <h2><a href="single.php?articleId=<?= htmlspecialchars($article->id);?>"><?= htmlspecialchars($article->title);?></a></h2>
+        <h2><a href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->id);?>"><?= htmlspecialchars($article->title);?></a></h2>
             <p><?= htmlspecialchars($article->content);?></p>
             <p><?= htmlspecialchars($article->author);?></p>
             <p>Créé le : <?= htmlspecialchars($article->createdAt);?></p>
